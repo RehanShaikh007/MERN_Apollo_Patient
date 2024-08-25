c1ae614053c2ae11ec9378bbc733c9472d860cf8
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2024-08-25 11:56:13 +0530

    og jwt file

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,21 +1,14 @@
 export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
-  const secure = process.env.NODE_ENV === 'production'; 
-  res
-    .status(statusCode)
-    .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: secure, // Ensures the cookie is only sent over HTTPS in production
-      sameSite: secure ? "strict" : "lax",
-    })
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
+      const token = user.generateJsonWebToken();
+      const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
+      res.status(statusCode).cookie(cookieName, token, {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
+        httpOnly: true,
+        
+      }).json({
+        success: true,
+        message,
+        user,
+        token,
+      });
 };
